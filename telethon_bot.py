188ecb8239baf767f3d9fc29c3ebc5569a20d1d2
Neldev2000 <nelsonandresvivaslobo@gmail.com>
2023-05-25 21:06:57 +0000

    Solved the await event inside function

--- telethon_bot.py
+++ telethon_bot.py
@@ -19,21 +19,26 @@ api = {
     'token' : os.environ.get('API_TOKEN')
 }
 # Create a Telegram client
-client = telethon.TelegramClient('my_username', api_id=api['id'], api_hash=api['hash'])
+async def main():
+    client = telethon.TelegramClient('my_username', api_id=api['id'], api_hash=api['hash'])
 
-# Login to Telegram
-await client.connect()
+    # Login to Telegram
+    await client.connect()
 
-# Register a handler for incoming messages
-@client.on(NewMessage)
-async def handler(event):
-    # Check the message text
-    if event.text == 'reporte':
-        # Send a message back to the user
-        await event.respond('Hello!')
-    elif event.text == 'goodbye':
-        # Send a message back to the user
-        await event.respond('Goodbye!')
+    # Register a handler for incoming messages
+    @client.on(NewMessage)
+    async def handler(event):
+        # Check the message text
+        if event.text == 'reporte':
+            # Send a message back to the user
+            await event.respond('Hello!')
+        elif event.text == 'goodbye':
+            # Send a message back to the user
+            await event.respond('Goodbye!')
 
-# Close the client
-client.disconnect()
+    # Close the client
+    await client.disconnect()
+if __name__ == '__main__':
+    # Run the main function
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
